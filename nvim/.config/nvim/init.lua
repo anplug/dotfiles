d33957d1c481f9d8445bafdc1d69e9a102f07a6b
Anton Plugin <anton.plugin@relax-gaming.com>
2024-03-10 18:29:56 +0100

    Use mason for managing lsp packages

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -15,6 +15,7 @@ vim.o.termguicolors = true
 vim.o.swapfile = false
 
 vim.opt.hidden = false -- no hiden opened buffers
+vim.opt.completeopt = { "menu" }
 
 vim.wo.colorcolumn = '80,100,108'
 
@@ -33,27 +34,79 @@ require('packer').startup(function()
   use { 'ntpeters/vim-better-whitespace' }
   use { 'editorconfig/editorconfig-vim' }
 
-  use { 'neovim/nvim-lspconfig' }
-
   use { 'slim-template/vim-slim' }
 
   use {
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons'
   }
+
+  -- LSP
+  use { "neovim/nvim-lspconfig" }
+  use { "williamboman/mason.nvim", config = true }
+  use { "williamboman/mason-lspconfig.nvim" }
 end)
 
 -- Laguage servers
 
+require('mason').setup()
+require('mason-lspconfig').setup {
+  ensure_installed = { 'ruby_ls', "rust_analyzer", 'clangd', 'lua_ls' }
+}
+
 local nvim_lsp = require('lspconfig')
 
-vim.api.nvim_set_keymap('n', '<C-i>',
-  ':lua vim.diagnostic.open_float()<CR>', {silent = true}
-)
-
-nvim_lsp.solargraph.setup{}
+nvim_lsp.ruby_ls.setup{}
 nvim_lsp.rust_analyzer.setup{}
 nvim_lsp.clangd.setup{}
+nvim_lsp.lua_ls.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim', 'use' }
+      }
+    }
+  }
+}
+
+vim.api.nvim_set_keymap('n', '<C-i>', ':lua vim.diagnostic.open_float()<CR>', {silent = true})
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+-- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+-- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
+vim.api.nvim_set_keymap('i', '<C-e>', "<C-x><C-o>", {noremap = true})
+
 
 -- Colorschemes
 -- Dark: one, afterglow, challenger_deep, dogrun, gotham
