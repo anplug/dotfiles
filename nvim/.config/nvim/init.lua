9e18d014ac2bdbe912507100006a1e2aa9e2c855
anplug <anplug@gmail.com>
2026-01-03 03:01:32 +0100

    Replace packer with lazy vim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,9 @@
 ---- disable netrw and use nvim-tree instead
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+-- Needed for LazyVim
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 vim.o.number = true
 vim.o.hlsearch = true
@@ -19,42 +22,64 @@ vim.opt.completeopt = { "menu" }
 
 vim.wo.colorcolumn = '80,100,108'
 
-require('packer').startup(function()
-  use { 'wbthomason/packer.nvim' }
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
 
-  use { 'rafi/awesome-vim-colorschemes' }
-  use { 'matze/vim-move' }
-  use { 'tpope/vim-fugitive' }
-  use { 'tpope/vim-endwise' }
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    { 'rafi/awesome-vim-colorschemes' },
+    { 'matze/vim-move' },
+    { 'tpope/vim-fugitive' },
+    { 'tpope/vim-endwise' },
 
-  use { 'vijaymarupudi/nvim-fzf' }
-  use { 'ibhagwan/fzf-lua' }
+    { 'vijaymarupudi/nvim-fzf' },
+    { 'ibhagwan/fzf-lua' },
 
-  use { 'ntpeters/vim-better-whitespace' }
-  use { 'editorconfig/editorconfig-vim' }
+    { 'ntpeters/vim-better-whitespace' },
+    { 'editorconfig/editorconfig-vim' },
 
-  use { 'slim-template/vim-slim' }
+    { 'slim-template/vim-slim' },
 
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons'
-  }
-
-  -- LSP
-  use { "neovim/nvim-lspconfig" }
-  use { "hrsh7th/cmp-nvim-lsp" }
-  use { "hrsh7th/cmp-buffer" }
-  use { "hrsh7th/cmp-path" }
-  use { "hrsh7th/cmp-cmdline" }
-  use { "hrsh7th/nvim-cmp" }
-
-  -- use { "github/copilot.vim" }
-  -- use {
-  --   "CopilotC-Nvim/CopilotChat.nvim",
-  --   branch = "main",
-  --   requires = "nvim-lua/plenary.nvim"
-  -- }
-end)
+    { 'kyazdani42/nvim-tree.lua',
+       dependencies = { 'kyazdani42/nvim-web-devicons' }
+    },
+    { "neovim/nvim-lspconfig" },
+    { "hrsh7th/nvim-cmp",
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline"
+      }
+    },
+   -- { "github/copilot.vim" },
+   -- { "CopilotC-Nvim/CopilotChat.nvim",
+   --   branch = "main",
+   --   dependencies = { "nvim-lua/plenary.nvim" }
+   -- }
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+--
+-- end)
 
 -- Autocomplete (nvim-cmp)
 --
